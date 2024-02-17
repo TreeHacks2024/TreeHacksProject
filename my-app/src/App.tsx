240d597d48e02166921a6ae3f3ab268a42d27398
Kevin Phouisangiem <kevinpho970@gmail.com>
2024-02-17 04:37:27 -0800

    moved users

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Home from "./Components/Home";
 
 function App() {
   return (
@@ -12,17 +13,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-          <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+        <Home />
         <a
           className="App-link"
           href="https://reactjs.org"
